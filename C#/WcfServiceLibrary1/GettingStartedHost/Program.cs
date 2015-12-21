f17d58e3a748443cb78ddd8cf438e25ff4580544
Aderick.k <aderick.k@gmail.com>
2015-12-22 03:09:58 +0700

    Example for C# connection

---
+++ C#/WcfServiceLibrary1/GettingStartedHost/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+using System.ServiceModel.Description;
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted");
+            ServiceHost selfHost = new ServiceHost(typeof(Service2), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IServiceContract), new WSHttpBinding(), "HelloWorldService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("the service is ready.");
+                Console.WriteLine("Press <enter> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch(CommunicationException ex)
+            {
+                Console.WriteLine("An exception occured: {0}", ex.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ C#/WcfServiceLibrary1/WcfServiceLibrary1/IServiceContract.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Microsoft.Samples.XmlRpc;
+
+namespace WcfServiceLibrary1
+{
+    [ServiceContract]
+    public interface IServiceContract
+    {
+        [OperationContract(Action = "Hello")]
+        string Hello(string name);
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IServiceContract> cf = new ChannelFactory<IServiceContract>(
+                new WebHttpBinding(), "http://www.example.com/xmlrpc");
+
+            cf.Endpoint.EndpointBehaviors.Add(new XmlRpcEndpointBehavior());
+
+            IServiceContract client = cf.CreateChannel();
+
+            string answer = client.Hello("world");
+
+        }
+
+    }
+}
