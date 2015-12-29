5e750809e2bdb4ca6e4cd5fcf371570ba8e39e11
Aderick.k <aderick.k@gmail.com>
2015-12-29 18:23:44 +0700

    Current work. Still error.

---
+++ C#/WcfNetworkServer/CalculatorServiceLibrary/ICalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using CookComputing.XmlRpc;
+
+namespace CalculatorServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    public interface ICalculatorService
+    {
+        [XmlRpcMethod("math.Add", Description = "add two integers")]
+        int Add(int a, int b);
+    }    
+
+    public interface ICalculatorServiceProxy : ICalculatorService, IXmlRpcProxy
+    {
+    }
+}
---
+++ C#/WcfNetworkServer/WcfNetworkServer/IWcfService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace WcfNetworkServer
+{
+    [ServiceContract]
+    public interface IWcfService 
+    {
+        [OperationContract(Action = "WcfService.AppendString")]
+        string AppendString(string value);       
+    }   
+}
