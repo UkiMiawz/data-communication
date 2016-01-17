9848d8652a890362dbf4dc598279cf19925d767f
Aderick.k <aderick.k@gmail.com>
2016-01-18 02:30:08 +0700

    CHECKPOINT 3: Major changes in server.

---
+++ C#/NetworkService-XmlrpcNet/CSharpRpcServer/CSharpRpcServer.cs
@@ -0,0 +1,47 @@
+﻿using CookComputing.XmlRpc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class CSharpRpcServer : MarshalByRefObject
+{
+    public static void Main(string[] args)
+    {
+        string input;
+
+        IDictionary props = new Hashtable();
+        props["name"] = "MyHttpChannel";
+        props["port"] = 1090;
+
+        HttpChannel channel = new HttpChannel(
+            props,
+            new XmlRpcClientFormatterSinkProvider(),
+            new XmlRpcServerFormatterSinkProvider());
+        ChannelServices.RegisterChannel(channel, false);
+
+        RemotingConfiguration.RegisterWellKnownServiceType(
+            typeof(CSharpRpcServer),
+            "xml-rpc-example/xmlrpc",
+            WellKnownObjectMode.Singleton);
+
+        Console.WriteLine("=====Xmlrpc-Server=====");
+        Console.WriteLine("type 'exit' to end server");
+        do
+        {
+            input = Console.ReadLine().ToLower();
+        }
+        while (input != "exit");
+    }
+
+    [XmlRpcMethod("HelloWorld.hello")]
+    public string Hello(string input)
+    {
+        return "Hello " + input;
+    }
+}
