a025147fa9f5ac381575db95c3737278facec0b6
Duy Khanh <duykhanhgian0510@gmail.com>
2016-01-18 22:42:50 +0100

    change ICsRpcClient

--- C#/CSharp-XmlRpcNet/XmlrpcHelper/ICSharpRpcClient.cs
+++ C#/CSharp-XmlRpcNet/XmlrpcHelper/ICSharpRpcClient.cs
@@ -9,5 +9,19 @@ public interface ICSharpRpcClient : IXmlRpcProxy
 {
     [XmlRpcMethod("Hello")]
     string HelloWorld(string input);
+
+    [XmlRpcMethod("Server.getMachines")]
+    XmlRpcStruct[] getMachines();
+
+    [XmlRpcMethod("Server.serverShutDownFromClient")]
+    void serverShutDownFromClient();
+
+    [XmlRpcMethod("Election.leaderElection")]
+    string leaderElection(string ipadress);
+
+    [XmlRpcMethod("RegisterHandler.getIpMaster")]
+    string getIpMaster(string ipaddress);
+
+
 }
 
